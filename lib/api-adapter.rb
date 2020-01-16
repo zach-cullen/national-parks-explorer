c3800d85dbeff2be1731939cb54945518a1bfde9
Zach Cullen <zacullen@gmail.com>
2020-01-16 09:43:28 -0500

    create initial cli and connect to nps api

--- lib/api-adapter.rb
+++ lib/api-adapter.rb
@@ -1,1 +1,15 @@
-#gets information from national parks api
+# service class that gets information from national parks api
+
+class APIAdapter
+    BASE_URL = "https://developer.nps.gov/api/v1/" 
+
+    def self.get_parks_by_state(stateCode)
+        response = RestClient.get("#{BASE_URL}parks?stateCode=#{stateCode}&api_key=#{ENV['NPS_API_KEY']}")
+        JSON.parse(response.body)["data"] # returns array of hashes containing info about each park
+    end
+
+    def self.list_parks(array, stateCode)
+        puts "Here are all of the national parks in #{stateCode}:"
+        array.each.with_index(1) {|park, index| puts "#{index}. #{park["fullName"]}"}
+    end
+end
--- lib/cli.rb
+++ lib/cli.rb
@@ -1,5 +1,15 @@
 #command line interface
+require_relative 'api-adapter'
 
 class CLI
+    attr_accessor :current_state
 
+    def initialize
+        puts "Welcome to National Parks Explorer!"
+        puts "Enter a two digit state code to see a list of national parks in that state"
+        user_input = gets.strip.upcase
+        @current_state = user_input
+        puts "Looking up parks in #{@current_state}..."
+        APIAdapter.list_parks(APIAdapter.get_parks_by_state(@current_state), @current_state)
+    end 
 end
