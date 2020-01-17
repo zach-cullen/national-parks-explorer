c2fef914648eb0f2cfc57cc980aac0b42906c039
Zach Cullen <zacullen@gmail.com>
2020-01-17 10:53:50 -0500

    separate load and list parks functions, refactor main_menu

--- lib/national_parks_explorer/cli.rb
+++ lib/national_parks_explorer/cli.rb
@@ -4,13 +4,13 @@ class NPExplorer::CLI
   def start
     system('clear') # clears terminal
 
-    puts "Welcome to National Parks Explorer! \n\n"
+    puts "Welcome to National Parks Explorer!"
     NPExplorer::APIAdapter.create_all_states
     main_menu
   end 
 
   def main_menu
-    puts "Enter the name of a US state or territory or its state code to see a list of national parks in that state."
+    puts "\nEnter the name of a US state or territory or its state code to see a list of national parks in that state."
     puts "To see a list of states and territories and their codes enter 'states'"
     puts "To exit this program, enter 'exit'"
     
@@ -19,7 +19,7 @@ class NPExplorer::CLI
 
   def main_menu_input
     user_input = gets.strip
-    @current_state = user_input.upcase
+    # @current_state = user_input.upcase
 
     if user_input.downcase == "states"
       list_states_and_territories
@@ -27,8 +27,19 @@ class NPExplorer::CLI
       main_menu
     elsif user_input.downcase == "exit"
       goodbye
-    elsif represents_a_state?(user_input)
-      load_and_list_parks
+    elsif is_a_state_code?(user_input)
+      reformatted = user_input.upcase #turns ny or Ny into NY
+      state = self.class.find_state_by_code(reformatted)
+      load_parks_by_state(state)
+      list_parks_in_state(state)
+      #list_parks_in_state(state)
+      main_menu
+    elsif is_a_state_name?(user_input)
+      reformatted = user_input.split(" ").each{|str| str.capitalize!}.join(" ") #turns new york or New york into New York
+      state = self.class.find_state_by_name(reformatted)
+      load_parks_by_state(state)
+      list_parks_in_state(state)
+      #list_parks_in_state(state)
       main_menu
     else 
       puts "Sorry, that is not a valid entry. Please follow the instructions below:"
@@ -36,24 +47,32 @@ class NPExplorer::CLI
     end
   end
 
-  def represents_a_state?(string)
-    state_names = ALL_STATES.map {|state| state[:name]}
-    state_codes = ALL_STATES.map {|state| state[:code]}
-    
-    if state_names.include?(string.split(" ").each{|str| str.capitalize!}.join(" "))
-      @current_state = ALL_STATES.find {|state| state[:name] == string.split(" ").each{|str| str.capitalize!}.join(" ")}[:code]
-      return true
-    elsif state_codes.include?(string.upcase)
-      @current_state = string.upcase 
-      return true
-    else
-      return false
-    end
+  def is_a_state_code?(string)
+    reformatted = string.upcase #turns ny or Ny into NY
+    NPExplorer::State.all_state_codes.include?(reformatted)
   end
 
-  def load_and_list_parks
-    puts "Looking up parks in #{@current_state}..."
-    NPExplorer::APIAdapter.list_parks(NPExplorer::APIAdapter.make_parks_from_data(@current_state), @current_state)
+  def is_a_state_name?(string)
+    reformatted = string.split(" ").each{|str| str.capitalize!}.join(" ") #turns new york or New york into New York
+    NPExplorer::State.all_state_names.include?(reformatted)
+  end
+
+  def self.find_state_by_code(state_code)
+    NPExplorer::State.all.find {|state| state.code == state_code}
+  end
+
+  def self.find_state_by_name(state_name)
+    NPExplorer::State.all.find {|state| state.name == state_name}
+  end
+
+  def load_parks_by_state(state)
+    puts "Looking up parks in #{state.name}...\n\n"
+    NPExplorer::APIAdapter.make_parks_from_data(state.code)
+  end
+
+  def list_parks_in_state(state)
+    puts "National Parks in #{state.name}:"
+    state.parks.each.with_index(1) {|park, index| puts "#{index}. #{park.name}"}
   end
 
   def list_states_and_territories
