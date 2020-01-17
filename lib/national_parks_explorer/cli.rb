e5867dce4c448dea88b335bbe999443a0ddbf649
Zach Cullen <zacullen@gmail.com>
2020-01-17 12:03:02 -0500

    created sub menu for parks list

--- lib/national_parks_explorer/cli.rb
+++ lib/national_parks_explorer/cli.rb
@@ -1,6 +1,6 @@
 class NPExplorer::CLI
   attr_accessor :current_state
-  
+
   def start
     system('clear') # clears terminal
 
@@ -17,9 +17,17 @@ class NPExplorer::CLI
     main_menu_input
   end
 
+  def sub_menu_parks
+    puts "\nEnter a number from list above to get information on a specific park"
+    puts "Enter 'tour' for descriptions of all #{@current_state.name} national parks"
+    puts "To return to main menu enter 'main'"
+    puts "To exit this program, enter 'exit'"
+
+    sub_menu_parks_input
+  end
+
   def main_menu_input
     user_input = gets.strip
-    # @current_state = user_input.upcase
 
     if user_input.downcase == "states"
       list_states_and_territories
@@ -29,23 +37,48 @@ class NPExplorer::CLI
       goodbye
     elsif is_a_state_code?(user_input)
       reformatted = user_input.upcase #turns ny or Ny into NY
-      state = self.class.find_state_by_code(reformatted)
-      load_parks_by_state(state)
-      list_parks_in_state(state)
-      #list_parks_in_state(state)
-      main_menu
+      @current_state = self.class.find_state_by_code(reformatted)
+      load_parks_by_state(@current_state)
+      list_parks_in_state(@current_state)
+      sub_menu_parks
     elsif is_a_state_name?(user_input)
       reformatted = user_input.split(" ").each{|str| str.capitalize!}.join(" ") #turns new york or New york into New York
-      state = self.class.find_state_by_name(reformatted)
-      load_parks_by_state(state)
-      list_parks_in_state(state)
-      #list_parks_in_state(state)
-      main_menu
+      @current_state = self.class.find_state_by_name(reformatted)
+      load_parks_by_state(@current_state)
+      list_parks_in_state(@current_state)
+      sub_menu_parks
     else 
       puts "Sorry, that is not a valid entry. Please follow the instructions below:"
       main_menu
     end
   end
+
+  def sub_menu_parks_input
+    user_input = gets.strip
+
+    if user_input.to_i > 0 && user_input.to_i <= @current_state.parks.length
+      park = @current_state.parks[user_input.to_i - 1]
+      puts "\n#{park.name}"
+      puts "\n#{park.description}"
+      sub_menu_parks
+    elsif user_input.downcase == "tour"
+      @current_state.parks.each.with_index(1) do |park, index|
+        puts "\n#{index}. #{park.name}"
+        puts "\n#{park.description}"
+      end 
+      sub_menu_parks
+    elsif user_input == "main"
+      main_menu
+    elsif user_input == "exit"
+      goodbye
+    else
+      puts "Sorry that is an invalid entry, follow the instructions below:"
+      sub_menu_parks
+    end
+  end
+
+
+
 
   def is_a_state_code?(string)
     reformatted = string.upcase #turns ny or Ny into NY
@@ -78,8 +111,6 @@ class NPExplorer::CLI
   def list_states_and_territories
     puts "\nSTATES: "
     ALL_STATES.each {|state| puts "#{state[:code]} - #{state[:name]}"}
-    puts "\nTERRITORIES: "
-    ALL_TERRITORIES.each {|terr| puts "#{terr[:code]} - #{terr[:name]}"}
   end
 
   def goodbye
