5a82c3d1da0518516e5d40dde5a8b6b2816657c4
Zach Cullen <zacullen@gmail.com>
2020-01-19 17:04:13 -0500

    html unicode decimals replaced with special characters

--- lib/national_parks_explorer/park.rb
+++ lib/national_parks_explorer/park.rb
@@ -4,14 +4,14 @@ class NPExplorer::Park
   @@all = []
 
   def initialize(name:, park_code:, state_code_array:, description:, location:, weather:)
-    @name = name
+    @name = replace_special_characters(name)
     @park_code = park_code
     @description = description
     @weather = weather
     @location = location
     @states = []
     state_code_array.split(',').each {|code| @states << NPExplorer::State.find_state_by_code(code)}
-    @states.each {|state| self.save_to_state(state)}
+    #@states.each {|state| self.save_to_state(state)}
     self.save
   end
 
@@ -19,12 +19,25 @@ class NPExplorer::Park
     @@all << self
   end
 
-  def save_to_state(state)
-    state.parks << self
-  end
+  # def save_to_state(state)
+  #   state.parks << self
+  # end
 
   def self.all
     @@all
   end
+
+  def replace_special_characters(string)
+    if string.include?("&#241;")
+      string = string.gsub("&#241;", "ñ")
+    elsif string.include?("&#257;")
+      string = string.gsub("&#257;", "ā")
+    elsif string.include?("&#333;")
+      string = string.gsub("&#333;", "ō")
+    else 
+      string
+    end
+  end
+  
 
 end
