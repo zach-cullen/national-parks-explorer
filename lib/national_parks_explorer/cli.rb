28c05c94e0a17ef04d403171e815ba18ef5f3518
Zach Cullen <zacullen@gmail.com>
2020-01-19 11:23:32 -0500

    added sub_menu_park_details

--- lib/national_parks_explorer/cli.rb
+++ lib/national_parks_explorer/cli.rb
@@ -1,5 +1,5 @@
 class NPExplorer::CLI
-  attr_accessor :current_state
+  attr_accessor :current_state, :current_park
 
   def start
     system('clear') # clears terminal
@@ -46,8 +46,18 @@ class NPExplorer::CLI
 
   def sub_menu_parks
     puts "\n"
-    puts "'list' - to go back to the list of parks in #{@current_state.name}\n"
+    puts "Enter the NUMBER of a park above for more information"
     puts "'tour' - See descriptions of all national parks in #{@current_state.name}"
+    puts "Enter a STATE name or code to see a list of parks in that state"
+    puts "'states' - See a list of states and territories"
+    puts "'exit' - Exit this program"
+
+    sub_menu_parks_input
+  end
+
+  def sub_menu_park_details
+    puts "\n"
+    puts "'list' - to go back to the list of parks in #{@current_state.name}\n"
     puts "Explore another STATE by entering it's name or state code"
     puts "'states' - See a list of states and territories"
     puts "'exit' - Exit this program"
@@ -61,22 +71,18 @@ class NPExplorer::CLI
     if user_input.to_i > 0 && user_input.to_i <= @current_state.parks.length
       park = @current_state.parks[user_input.to_i - 1]
       display_park_information(park)
-      sub_menu_parks #change to detailed_menu
+      sub_menu_park_details
     elsif user_input.downcase == "tour"
-      @current_state.parks.each.with_index(1) do |park, index|
-        puts "\n#{index}. #{park.name}"
-        puts "\n#{park.description}\n\n"
-        sleep(0.01)
-      end 
+      tour_of_state
       sub_menu_parks
     elsif user_input.downcase == "list"
       list_parks_in_state(@current_state)
       sub_menu_parks
-    elsif user_input.downcase == "exit"
-      goodbye
     elsif user_input.downcase == "states"
       list_states_and_territories
       main_menu
+    elsif user_input.downcase == "exit"
+      goodbye
     elsif is_a_state_code?(user_input)
       reformatted = user_input.upcase #turns ny or Ny into NY
       @current_state = NPExplorer::State.find_state_by_code(reformatted)
@@ -123,8 +129,16 @@ class NPExplorer::CLI
     NPExplorer::State.all.each {|state| puts "#{state.code} - #{state.name}" ; sleep(0.02)}
   end
 
+  def tour_of_state
+    @current_state.parks.each.with_index(1) do |park, index|
+      puts "\n#{index}. #{park.name.upcase}"
+      puts "\n#{park.description}\n\n"
+      sleep(0.5)
+    end 
+  end
+
   def display_park_information(park)
-    puts "\n#{park.name}\n"
+    puts "\n#{park.name.upcase}\n"
     puts "\nDescription:\n"
     puts "#{park.description}"
     puts "\nLocation:\n"
